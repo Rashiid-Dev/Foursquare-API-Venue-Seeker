c7ca143efe0747ecdc0f6f44051f803e7048c9a9
Abdirashiid Jama <rashiid.py@gmail.com>
2019-03-28 11:11:11 +0000

    Adding the python files

---
+++ Foursquare.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+url = 'https://api.foursquare.com/v2/venues/explore'
+
+CLIENT_ID = 'ULD1MG0DG1Y1EB2APLJ4XZWIDHZ2OI4OYLUKULPRVVSIJJC0'
+CLIENT_SECRET = 'P2CHGT1SEU0EXKVQZCUJPCVQWXBS5W0I0UTUQ0YLAHCJTQHK'
+print("\nWelcome to Venue Seeker with Foursquare\n")
+place = input("Where would you like to search? \n - ")
+Query = input("\nWhat item would you like to search for? \n - ")
+params = dict(
+    client_id='ULD1MG0DG1Y1EB2APLJ4XZWIDHZ2OI4OYLUKULPRVVSIJJC0',
+    client_secret='P2CHGT1SEU0EXKVQZCUJPCVQWXBS5W0I0UTUQ0YLAHCJTQHK',
+    v='20180323',
+    near=place,
+    query=Query,
+    limit=1
+)
+
+response = requests.get(url=url, params=params)
+data = json.loads(response.text)
+init = (data['response'])
+print(init[1])
+# for items in data['response']:
+#     print(items)
